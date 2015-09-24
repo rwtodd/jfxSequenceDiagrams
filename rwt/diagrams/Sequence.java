55a93326d59d8e9a7038e02c8349fe2be23012ae
Richard Todd <waywardcoder@users.noreply.github.com>
2015-09-24 16:47:24 -0500

    got the basic layout of the main frame

---
+++ rwt/diagrams/Sequence.java
@@ -0,0 +1,25 @@
+package rwt.diagrams;
+
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.scene.layout.Pane;
+
+public class Sequence extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+       primaryStage.setTitle("Easy Sequence Diagrams");
+       Pane myPane = (Pane)FXMLLoader.load(getClass().getResource
+    ("sequence_diagrams.fxml"));
+       Scene myScene = new Scene(myPane);
+       primaryStage.setScene(myScene);
+       primaryStage.show();
+    }
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
