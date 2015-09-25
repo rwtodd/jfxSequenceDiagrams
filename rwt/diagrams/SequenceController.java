e5cd9319d065883219ea2e8fb67e6fdec242d9a8
Richard Todd <waywardcoder@users.noreply.github.com>
2015-09-24 22:37:08 -0500

    added a controller class.

---
+++ rwt/diagrams/SequenceController.java
@@ -0,0 +1,25 @@
+package rwt.diagrams;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+
+
+public class SequenceController implements Initializable {
+
+  @FXML private TextArea sourceArea;
+  @FXML private Pane drawingArea;
+
+
+  public void initialize(java.net.URL l, java.util.ResourceBundle r) {
+     sourceArea.textProperty().addListener( (obs,old,cur) -> {
+        /* need to set a timer here, and parse "cur" if the timer goes off */
+     }); 
+  }
+
+  public void setError(bool isError) {
+    sourceArea.getStyleClass().removeAll("sourceError","sourceNormal");
+    sourceArea.getStyleClass().add(isError ? "sourceError" : "sourceNormal" );
+  }
+}
