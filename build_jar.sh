82dcb591b01fde547bc6176e940a30e0ced4dd16
Richard Todd <richard@waywardcode.com>
2016-06-18 19:21:48 -0500

    brought up to date with SVN

--- build_jar.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# nothing fancy... I hate that the standard build tools (Ant, Maven, etc) are 
-# all so full of dependencies.  A simple tool is best, in my opinion.  So for
-# the moment, for utilities, I stick with a 2-line shell script. 
-
-javac -cp . rwt/diagrams/sequence/*.java rwt/diagrams/Sequence.java
-find rwt -type f \! -name '*.java' | xargs jar cvfm sequence_diagrams.jar Manifest.txt
-
